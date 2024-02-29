3612fa9c9b7d691dba0d97929a4d79dab3ff4c8f
S3rdna <aaguilar8079@gmail.com>
2024-02-29 05:26:21 -0800

    tkinter started

--- main.py
+++ main.py
@@ -12,29 +12,16 @@ class Filament:
     def setStartWeight(self,new_weight):
         self.start_weight = new_weight
 
-    def getBrand():
-        return self.brand
-
-    def getColor():
-        return self.color
-
-    def getMaterial():
-        return self.material
-
-    def getWeight():
-        return self.start_weight
-
     def __str__(self):
         return 'Material: {}\n\tManufacturer: {}\n\tColor: {}\n\tWeight: {} '.format(self.material,self.brand,self.color,self.start_weight)
 
 # Write filament to db
 # TODO: WHY DONT THIS WRITE TO .csv?
 def writeToDB(fil):
-    print(fil)
+    #print(fil)
     with open('filamentDB.csv', 'a', newline='') as csvfile:
-        writer = csv.writer(csvfile, delimiter=',')
-        for fil in stock: 
-            writer.writerow([fil.getBrand,fil.getColor,fil.getMaterial,fil.getWeight])
+        writer = csv.writer(csvfile, delimiter=',') 
+        writer.writerow([fil.brand,fil.color,fil.material,fil.start_weight])
 
 # Get all data from db and print on screen 
 def readFromDB():
@@ -56,12 +43,12 @@ transTealPETG = Filament('Bambu Labs', 'Transparent Teal','PETG', 1000)
 transPinkPETG = Filament('Bambu Labs', 'Transparent Pink','PETG', 1000)
 jadeWhitePLA = Filament('Bambu Labs', 'Jade White','PLA', 1000)
 
-writeToDB(lilacPLA)
-writeToDB(whiteABS)
-writeToDB(armyGreenPETG)
-writeToDB(transPinkPETG)
-writeToDB(transTealPETG)
-writeToDB(jadeWhitePLA)
+#writeToDB(lilacPLA)
+#writeToDB(whiteABS)
+#writeToDB(armyGreenPETG)
+#writeToDB(transPinkPETG)
+#writeToDB(transTealPETG)
+#writeToDB(jadeWhitePLA)
 
 # Return data in gui format 
 
@@ -70,23 +57,48 @@ def print_content():
     content = text_area.get("1.0", tk.END)
     print(content)
 
+def handleAddFilamentBtn():
+    manufacturer = brandEntry.get()
+    color = colorEntry.get()
+    material = materialEntry.get()
+    weight = weightEntry.get()
+
+    writeToDB(Filament(manufacturer,color,material,weight))
+    
 # Create the main window
 root = tk.Tk()
-root.title("Tkinter Example")
+root.title("Filament Info")
 
 # Set the size of the window
-root.geometry("400x250")
+root.geometry("800x450")
 
-# Create a Text Widget for the text area
-text_area = tk.Text(root, height=10, width=50)
-text_area.pack()
+# create frames 
+addFilFrame = tk.Frame(root)
+addFilFrame.pack(side=tk.LEFT,fill=tk.Y)
+infoFrame = tk.Frame(root)
+infoFrame.pack(side=tk.RIGHT,fill=tk.BOTH,expand=True)
 
-# Create a Button that will print the content of the text area
-print_button = tk.Button(root, text="Print Content", command=print_content)
-print_button.pack()
+# Create labels with info to infoFrame
+for index,val in enumerate(readFromDB()):
+    print(index,val)
+    temp = tk.Label(infoFrame,text=val)
+    temp.pack()
+
+# add entry fields to shite
+brandEntry = tk.Entry(addFilFrame,text='Manufacturer')
+colorEntry = tk.Entry(addFilFrame,text='Color')
+materialEntry = tk.Entry(addFilFrame,text='Material')
+weightEntry = tk.Entry(addFilFrame,text='Weight')
+
+brandEntry.pack()
+colorEntry.pack()
+materialEntry.pack()
+weightEntry.pack()
+
+# add to db Btn 
+add_btn = tk.Button(addFilFrame,text='Add Filament', command=handleAddFilamentBtn)
+add_btn.pack()
 
 # Start the GUI event loop
 root.mainloop()
 
-for i in readFromDB():
-    print(i)
