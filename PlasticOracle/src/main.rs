cfb4d1aa5036789908846bc9084a6cb7baaa5abc
S3rdna <aaguilar8079@gmail.com>
2024-07-27 12:37:38 -0700

    push

--- PlasticOracle/src/main.rs
+++ PlasticOracle/src/main.rs
@@ -19,20 +19,47 @@ enum MaterialModifier {
 struct Filament {
     material: MaterialType,
     modifier: MaterialModifier,
+    price: f32,
 }
 
 fn display_gui() {
-    use std::io::{self, Write};
-
-    io::stdout().write(b"this").expect("test");
     println!("Select Option");
     println!("1. Check Filament");
     println!("2. Add Filament");
 }
 
+#[derive(Debug)]
+enum UserInputError {
+    NoInputError,
+    WrongSizeInputError,
+    UserInputParseError,
+}
+
+fn get_user_input() -> Result<u8, UserInputError> {
+    use std::io::{self};
+
+    let mut input = String::new(); //create buffer to hold input
+    let _ = io::stdin().read_line(&mut input); // create stdin struct
+    input = input.trim().to_string(); //trim input
+
+    match input.len() {
+        0 => Err(UserInputError::NoInputError),
+        1 => match input.parse::<u8>() {
+            Err(_) => Err(UserInputError::UserInputParseError),
+            Ok(k) => Ok(k),
+        },
+        _ => Err(UserInputError::WrongSizeInputError),
+    }
+}
+
 fn main() {
-    //gui
-    display_gui();
-    //save data in file
-    //calculate remaining filament
+    loop {
+        display_gui(); //ui
+        match get_user_input() {
+            Ok(k) => println!("{k}"),
+            Err(e) => println!("{:?}", e),
+        }
+        //save data in file
+        //calculate remaining filament
+    }
 }
